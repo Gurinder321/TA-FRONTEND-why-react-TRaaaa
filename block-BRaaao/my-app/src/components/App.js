63f3e3f5714a7b401bf9e243365960d3a223326d
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-06-09 11:32:50 -0700

    DONE

---
+++ block-BRaaao/my-app/src/components/App.js
@@ -0,0 +1,39 @@
+function App(props) {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Articles />
+      <Footer />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  );
+}
+
+function Hero() {
+  return <img src="/images/birdy.png" />;
+}
+
+function Articles(props) {
+  return (
+    <div>
+      <img src="https://images.unsplash.com/photo-1648737963080-109bbf325c90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60" />
+      <h3>{props.title}</h3>
+    </div>
+  );
+}
+
+function Footer() {
+  return <h6>Copyright</h6>;
+}
+
+export default App;
