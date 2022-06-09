0840691166b9163b0f5a354996fc3dbe505337ab
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-06-09 12:29:02 -0700

    Dome

--- block-BRaaao/my-app/src/components/App.js
+++ block-BRaaao/my-app/src/components/App.js
@@ -1,39 +1,19 @@
-function App(props) {
+import Header from './Header';
+import Hero from './Hero';
+import Article from './Article';
+import Footer from './Footer';
+
+function App() {
   return (
     <>
       <Header />
-      <Hero />
-      <Articles />
+      <main className="wrapper">
+        <Hero />
+        <Article />
+      </main>
       <Footer />
     </>
   );
-}
-
-function Header() {
-  return (
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  );
-}
-
-function Hero() {
-  return <img src="/images/birdy.png" />;
-}
-
-function Articles(props) {
-  return (
-    <div>
-      <img src="https://images.unsplash.com/photo-1648737963080-109bbf325c90?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=700&q=60" />
-      <h3>{props.title}</h3>
-    </div>
-  );
-}
-
-function Footer() {
-  return <h6>Copyright</h6>;
 }
 
 export default App;
